c9e090f11211a9c8fa653c5b22e1fc6449f99cfb
LoiNguyen94 <loinh@mioapp.vn>
2022-06-16 11:46:58 +0700

    fb

--- libs/ui-shares/src/lib/main/layout.tsx
+++ libs/ui-shares/src/lib/main/layout.tsx
@@ -4,7 +4,6 @@ import styles from './layout.module.scss';
 import React, { ReactNode } from 'react';
 import TabBottom from './tab_bottom';
 import { useWindowSize } from '@monorepo/function-shares';
-import Head from 'next/head';
 
 export interface LayoutProps {
   children?: ReactNode;
@@ -25,16 +24,6 @@ export function MainLayout({
 
   return (
     <>
-      <Head>
-        <title>{title}</title>
-        <meta
-          name="viewport"
-          content="target-densitydpi=device-dpi, initial-scale=1.0, user-scalable=no"
-        />
-        <meta property="og:title" content={title} key="title" />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={photo} />
-      </Head>
       <div style={{ height: height }} className={styles['container']}>
         <div style={{ width: widthFixed }} className={styles['frame']}>
           {children}
--- libs/ui-shares/src/lib/main/layout_trans.tsx
+++ libs/ui-shares/src/lib/main/layout_trans.tsx
@@ -1,6 +1,5 @@
 import { useState, ReactNode, useEffect, memo } from 'react';
 import styles from './layout.module.scss';
-import Head from 'next/head';
 import { useWindowSize } from '@monorepo/function-shares';
 import TabBottom from './tab_bottom';
 import { useRouter } from 'next/router';
@@ -50,16 +49,6 @@ export const TransitionLayout = memo(
 
     return (
       <div>
-        <Head>
-          <title>{title}</title>
-          <meta
-            name="viewport"
-            content="target-densitydpi=device-dpi, initial-scale=1.0, user-scalable=no"
-          />
-          <meta property="og:title" content={title} key="title" />
-          <meta property="og:description" content={description} />
-          <meta property="og:image" content={photo} />
-        </Head>
         <div style={{ height: height }} className={styles['container']}>
           <div
             style={{ width: widthFixed }}
