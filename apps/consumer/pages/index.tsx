193a75c1527dc7e8d946a9fe87677d0e5be0894a
LoiNguyen94 <loinh@mioapp.vn>
2022-06-16 12:03:04 +0700

    last

--- apps/consumer/pages/index.tsx
+++ apps/consumer/pages/index.tsx
@@ -1,12 +1,12 @@
-import { Splash } from '@monorepo/ui-shares';
-import Home from './home';
-import Head from 'next/head';
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
 
-export function Index() {
+export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
-        <meta
+      <meta
           name="title"
           content="MacBook Pro M1 2020 13 inch | Trả góp 0%, ưu đãi hấp dẫn | Fptshop.com.vn"
         />
@@ -72,9 +72,60 @@ export function Index() {
         <meta property="fb:app_id" content="948243871965133" />
         <meta name="referrer" content="no-referrer-when-downgrade" />
       </Head>
-      <Home />
-    </div>
-  );
-}
 
-export default Index;
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
+
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h2>Documentation &rarr;</h2>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h2>Learn &rarr;</h2>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/canary/examples"
+            className={styles.card}
+          >
+            <h2>Examples &rarr;</h2>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h2>Deploy &rarr;</h2>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
+}
