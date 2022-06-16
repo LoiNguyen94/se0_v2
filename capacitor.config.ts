549c1cdd90acfa3cc4ba59893a21e1233b843b7f
LoiNguyen94 <loinh@mioapp.vn>
2022-06-16 14:38:50 +0700

    test

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'demo__seo__v2',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
